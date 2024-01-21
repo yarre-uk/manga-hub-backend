dfba144087b4549a802b3ce3e188a5f1f701c55b
Yarre <yaroslav.syvukha@gmail.com>
2024-01-21 22:22:33 +0100

    feat: add nest container to docker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 
+import { Public } from '@common/decorators';
+
 @Controller('app')
 export class AppController {
+  @Public()
   @Get('test')
   test() {
     return 'works!';
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,6 +4,22 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
+    let counter = 0;
+
+    // while (true) {
     await this.$connect();
+    // try {
+    //   await this.$connect();
+    //   break;
+    // } catch {
+    //   await new Promise((r) => setTimeout(r, 2000));
+    //   counter++;
+    //   console.log(`Retrying to connect to DB... ${counter}`);
+    //   if (counter > 10) {
+    //     console.error('DB connection failed');
+    //     break;
+    //   }
+    // }
+    // }
   }
 }
