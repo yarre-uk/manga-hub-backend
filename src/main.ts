c57f6df3452e696fab8e84edbabb08eb2f79e3f0
Yarre <yaroslav.syvukha@gmail.com>
2024-02-03 12:48:01 +0100

    fear: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
@@ -10,6 +11,20 @@ async function bootstrap() {
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors({
+      origin: true,
+      credentials: true,
+    });
+
+    const config = new DocumentBuilder()
+      .setTitle('Manga API')
+      .setBasePath('api')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+  }
 
   await app.listen(3000);
 }
